21c371956c5c936b5b457d1050a16c8b32d7fac7
Taha Lallali <110392985+Taha5510@users.noreply.github.com>
2022-11-28 21:44:16 +0000

    Delete project 1/htdocs directory

--- project 1/htdocs/php/getWikipedia.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$country = $_GET['country'];
-// The file_get_contents() reads a file into a string.
-$data = file_get_contents("http://api.geonames.org/wikipediaSearch?q=$country&maxRows=10&username=ShashAPI");
-print_r($data);
