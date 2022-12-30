700931f86872a9e96d39064654d36a205d444bb6
Taha Lallali <110392985+Taha5510@users.noreply.github.com>
2022-12-30 23:32:35 +0000

    Update getOcean.php

--- Project1/php/getOcean.php
+++ Project1/php/getOcean.php
@@ -2,5 +2,5 @@
 
 $lat = $_GET['lat'];
 $lng = $_GET['lng'];
-$data = file_get_contents("http://api.geonames.org/oceanJSON?lat=$lat&lng=$lng&radius=100&username=ShashAPI");
+$data = file_get_contents("http://api.geonames.org/oceanJSON?lat=$lat&lng=$lng&radius=200&username=ShashAPI");
 print_r($data);
